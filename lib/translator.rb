d8bd37bfa542c29b9be1657e8e98c50f13946fda
Christina Sohn <chsohn15@gmail.com>
2020-06-16 01:32:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,9 +6,8 @@ def load_library(file)
   final_hash = emoticons.each_with_object({}) do |(key,value), final_array|
     
     emoticons[key] = {:english => value[0], :japanese => value[1]}
-    binding.pry
   end
-
+binding.pry
 end
 
 def get_japanese_emoticon
