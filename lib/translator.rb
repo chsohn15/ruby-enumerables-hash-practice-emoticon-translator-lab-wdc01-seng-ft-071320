58ab34674b4372a9a8fb128959ea3cbf91b1d9cf
Christina Sohn <chsohn15@gmail.com>
2020-06-16 01:20:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,6 +4,9 @@ require 'pry'
 def load_library(file)
   emoticons = YAML.load_file(file)
   final_hash = emoticons.each_with_object({}) do |(key,value), final_array|
+    key = {}
+    {} << :english 
+    {} << :japanese
     value.each do |emojis|
       hash = Hash[value.collect {|emojis| [" ", emojis]}]
       
