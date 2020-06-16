a856656f8c7dab72d2e84ea633f9ba828f5ab620
Christina Sohn <chsohn15@gmail.com>
2020-06-16 02:20:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,7 @@ emoji_hash = load_library(file1)
 
 
 emoji_hash.each do |(k, v)|
-  if emoji_hash[k][value] == emoji
+  if emoji_hash[k][v] == emoji
     n = k
     return emoji_hash[n][:japanese]
   else 
